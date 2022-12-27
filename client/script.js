129bc8e793e63215d52f5defff0bf74b7c2177a0
arepatiesa <carlosmarcos2004@gmail.com>
2022-12-27 05:37:06 -0400

    update server url

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server to get bot's response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://maria-7962.onrender.com', {
     method: 'POST', 
     headers: {
       'Content-Type': 'application/json'
